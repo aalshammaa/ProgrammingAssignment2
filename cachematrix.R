ba3b279baab5ec88f768d96486175891306d9eb3
Abdullah Alshammaa <abdullah8844@gmail.com>
2017-10-02 13:16:03 -0400

    Revert "functional"
    This reverts commit 565fc5ebd9cb2172697e241cf248099961d10b87.

--- cachematrix.R
+++ cachematrix.R
@@ -4,21 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  inverse <- NULL
-  set <- function(m) {
-    x <<- m
-    inverse <<- NULL
-  }
-  get <- function() x
-  
-  setinverse <- function(b) inverse <<- b
-  
-  getinverse <- function() inverse
-  
-  list(set = set,
-       get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+
 }
 
 
@@ -26,13 +12,4 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  inverseMatrix <- x$getinverse()
-  if (!is.null(inverseMatrix)) {
-    message("Getting cached data")
-    return(inverseMatrix)
-  }
-  
-  data <- x$get()
-  inverseMatrix <- solve(data)
-  x$setinverse(inverseMatrix)
 }
